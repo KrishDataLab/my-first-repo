04693611f40253265e7169ef0cd01d02413acf72
KrishDataLab <mohanavenkatak@gmail.com>
2026-04-05 01:21:57 +0530

    Add files via upload

---
+++ python_basics_mohan.py
@@ -0,0 +1,25 @@
+#Task 1
+
+person_name = "krishna"
+
+age = 20
+
+height = 160.0
+
+is_student = True
+
+
+#Task 2
+print(f"person_name:{person_name} | age:{age}| height:{height} | is_student:{is_student} ")
+
+
+#Task 3
+age_in_months = 20*12
+
+age_in_days = 20*365
+
+Remainder = 20 // 7
+
+age_power = 20**2
+
+print(f"age_in_months:{age_in_months} | age_in_days:{age_in_days} | Remainder:{Remainder} | age_power:{age_power}")
